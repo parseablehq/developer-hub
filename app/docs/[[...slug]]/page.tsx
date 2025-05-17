84005f0e52df5ce2d0e7be0cc4893f82c21514ca
Nitish Tiwari <nitish@parseable.com>
2025-05-17 06:44:16 +0530

    further improvements (#15)

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -8,6 +8,10 @@ import {
 import { notFound } from 'next/navigation';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
 import { getMDXComponents } from '@/mdx-components';
+import { EditOnGitHub } from './page.client';
+
+const owner = 'parseablehq';
+const repo = 'developer-hub';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -15,6 +19,7 @@ export default async function Page(props: {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
+  const path = `content/docs/${page.file.path}`;
 
   const MDXContent = page.data.body;
 
@@ -22,6 +27,12 @@ export default async function Page(props: {
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
+      <div className="flex flex-row gap-2 items-center mb-4">
+        {/* <LLMCopyButton slug={params.slug} /> */}
+        <EditOnGitHub
+          url={`https://github.com/${owner}/${repo}/tree/main/${path}`}
+        />
+      </div>
       <DocsBody>
         <MDXContent
           components={getMDXComponents({
