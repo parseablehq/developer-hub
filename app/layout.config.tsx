063ae693420bb803d66898df63365462b373600b
Praveen K B <praveenbhat98@gmail.com>
2025-06-06 15:50:53 +0530

    fix: Fixed dark mode colours

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Link from "next/link";
 
 /**
  * Shared layout configurations
@@ -10,27 +11,25 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <>
-        <span className="flex items-center gap-2">
-          <span className="font-bold text-xl">Parseable Docs</span>
-        </span>
-      </>
+      <Link href="/docs" className="flex items-center gap-2">
+        <span className="font-bold text-xl">Parseable Docs</span>
+      </Link>
     ),
   },
   links: [
     {
-      text: 'Parseable Playground',
-      url: 'https://demo.parseable.com/login?q=eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiJ9',
+      text: "Parseable Playground",
+      url: "https://demo.parseable.com/login?q=eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiJ9",
       external: true,
     },
     {
-      text: 'API Documentation',
-      url: '/docs/api',
+      text: "API Documentation",
+      url: "/docs/api",
       external: false, // Internal link
     },
     {
-      text: 'Release Notes',
-      url: '/docs/release-notes',
+      text: "Release Notes",
+      url: "/docs/release-notes",
       external: false, // Internal link
     },
   ],
