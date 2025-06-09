e0d83a582634c7e751b1be4153cb25437ad679fb
AadishJ <jain.aadishj@gmail.com>
2025-06-09 14:56:02 +0530

    change vercel

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  basePath: '/docs',
+  assetsPrefix: '/docs',
   images: {
     domains: ['lh3.googleusercontent.com'], // Add other domains as needed
     dangerouslyAllowSVG: true,
-    basePath: '/docs',
-    assetsPrefix: '/docs',
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
